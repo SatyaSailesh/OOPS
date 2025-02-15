3c3e7de1a60d2e0d0c97df23f8620294e30f4f85
SatyaSailesh <kotla.sailesh2006@gmail.com>
2025-02-15 14:40:22 +0530

    Update Pyramid_Pattern.java

--- Pyramid_Pattern.java
+++ Pyramid_Pattern.java
@@ -1,1 +1,26 @@
 
+import java.util.*;
+public class Pyramid_Pattern
+{
+    
+    void Pattern(int n)
+    {
+        int i, j;
+        int num = 1;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= i; j++) 
+            {
+                System.out.print("*");
+                 num++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[])
+    {
+        int n = 6;
+        Pattern(n);
+    }
+}
