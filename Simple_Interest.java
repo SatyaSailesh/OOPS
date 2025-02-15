6e3fc0ee4e121b0034a6bdda116b82f6f62b956d
SatyaSailesh <kotla.sailesh2006@gmail.com>
2025-02-15 14:53:56 +0530

    Create Simple_Interest.java

---
+++ Simple_Interest.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class Simple_Interest {
+    public static void main(String args[])
+    {
+        /
+        float P = 10000, 
+        int R = 5, 
+        int T = 5;
+
+       
+        float SI = (P * T * R) / 100;
+        System.out.println("Simple interest = " + SI);
+    }
+}
